71de398c2e6832edbd5218df5e1ae3b3f515f1cd
takeit7 <ivan.sukhov.070707@gmail.com>
2025-09-25 18:00:49 +0300

    Add files via upload

--- #1.cpp
+++ #1.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-int main()
-{
-    setlocale(LC_ALL, "RU");
-    float i = 0;
-    std::cout << "Введите длину в см" << std::endl;
-    std::cin >> i;
-    std::cout << i / 100 << std::endl;
+using namespace std;
+float main() {
+	setlocale(LC_ALL, "RU");
+	float d = 0;
+	cout << "Укажите свой доход"<<endl;
+	cin >> d;
+	if (d < 50000) {
+		d = d - d * 0.13;
+		cout << "Доход с учетом налога: " << d;
+	}
+	if ((d > 50000) && (d <= 100000)) {
+		d = d - d * 0.2;
+		cout << "Доход с учетом налога: " << d;
+	}
+	if (d > 100000) {
+		d = d - d * 0.25;
+		cout << "Доход с учетом налога: " << d;
+	}
 	return 0;
 }
--- #2.cpp
+++ #2.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-int main()
-{
+using namespace std;
+
+int main() {
     setlocale(LC_ALL, "RU");
-    float i = 0;
-    float b = 0;
-    std::cout << "¬ведите длину" << std::endl;
-    std::cin >> i;
-    std::cout << "¬ведите ширину" << std::endl;
-    std::cin >> b;
-    std::cout << i * b << std::endl;
-	return 0;
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " - " << i * 10 << " руб"<<endl;
+    }
+    return 0;
 }
