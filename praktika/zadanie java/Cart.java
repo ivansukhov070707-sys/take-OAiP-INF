e605fc2d85d7c91dcfb5ff3a5e444b68e4b02dc1
takeit7 <ivan.sukhov.070707@gmail.com>
2025-12-15 13:51:37 +0300

    Add files via upload

---
+++ praktika/zadanie java/Cart.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+public class Cart {
+    private ArrayList<Product> products;
+    public Cart() {
+        this.products = new ArrayList<>();
+    }
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+    public void removeProduct(String name) {
+        products.removeIf(product -> product.getName().equalsIgnoreCase(name));
+    }
+    public double getTotal() {
+        double total = 0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+    public void printCart() {
+        System.out.println("=== Корзина ===");
+        if (products.isEmpty()) {
+            System.out.println("Корзина пуста");
+            return;
+        }
+        for (int i = 0; i < products.size(); i++) {
+            System.out.println((i + 1) + ". " + products.get(i).toString());
+        }
+        System.out.printf("Итого: %.1f руб.\n", getTotal());
+    }
+}
---
+++ praktika/zadanie java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Product laptop = new Product("Ноутбук", 75000.0);
+        Product mouse = new Product("Мышка", 1500.0);
+        Product keyboard = new Product("Клавиатура", 3000.0);
+        Cart cart = new Cart();
+        cart.addProduct(laptop);
+        cart.addProduct(mouse);
+        cart.addProduct(keyboard);
+        cart.printCart();
+        System.out.println("\nУдаляем: Мышка\n");
+        cart.removeProduct("Мышка");
+        cart.printCart();
+    }
+}
